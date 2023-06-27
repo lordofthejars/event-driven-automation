3d4f5d75232ea5b322f82e7b4b87c51c617a54a9
Alex Soto <asotobu@gmail.com>
2023-06-27 21:07:21 +0200

    Remove kafka

--- eda-quarkus-tester/src/main/java/org/acme/Producer.java
+++ eda-quarkus-tester/src/main/java/org/acme/Producer.java
@@ -11,38 +11,21 @@ import javax.ws.rs.core.MediaType;
 
 import org.acme.alertmanager.model.Alert;
 import org.acme.alertmanager.service.AlertService;
-import org.acme.model.Event;
 import org.acme.webhook.model.WebhookEvent;
 import org.acme.webhook.service.WebhookService;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Path("/")
 public class Producer {
 
-    @Channel("eda-topic")
-    Emitter<String> eventEmitter;
     @Inject
     @RestClient
     WebhookService webhookService;
     @Inject
     @RestClient
     AlertService alertService;
-
-    @GET
-    @Path("/kafka/greeting")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String sendKafkaEvent() throws JsonProcessingException {
-        Event testEvent = new Event("greeting", "Hello from Quarkus");
-        ObjectMapper mapper = new ObjectMapper();
-        String eventJson = mapper.writeValueAsString(testEvent);
-        eventEmitter.send(eventJson);
-        return eventJson;
-    }
 
     @GET
     @Path("/webhook/greeting")
